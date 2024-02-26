503bc7d7bfff822147c1656e005342df8d13663d
rodribp <rodrigobonilla177@gmail.com>
2024-02-25 22:54:59 -0600

    changing edit event to class Event method

--- src/controllers/events.controller.js
+++ src/controllers/events.controller.js
@@ -117,7 +117,7 @@ export const setEvent = (req, res) => {
         return;
     }
 
-    if (!event.setDescription(description) && description !== "") {
+    if (!event.setDescription(description)) {
         result.error = event.getError();
         res.status(400).json(result);
         return;
@@ -184,6 +184,7 @@ export const setEvent = (req, res) => {
 
 
 export const editEvent = (req, res) => {
+    const event = new Event();
     const result = {
         status: 0,
         error: ""
@@ -191,8 +192,8 @@ export const editEvent = (req, res) => {
 
     let fileNames = "";
 
-    const { title, location, description, start, timeStart, end, timeEnd, type, eventUrl, deleteIndicator } = req.body;
-    
+    let { title, location, description, start, timeStart, end, timeEnd, type, eventUrl, deleteIndicator } = req.body;
+
     try {
         fileNames = req.files;
     } catch (err) {
@@ -207,181 +208,77 @@ export const editEvent = (req, res) => {
 
     const uuid = req.params.uuid;
 
-    readFile("./src/json/events.json", "utf-8", (err, jsonString) => {
-        if (err) {
-            result.error = "Error reading file";
-            res.status(500).json(result);
-            return;
-        }
+    if (end === "") end = start;
 
-        const data = JSON.parse(jsonString);
+    if (!event.setUuid(uuid)) {
+        result.error = event.getError();
+        res.status(500).json(result);
+        return;
+    }
 
-        const foundIndex = data.findIndex((event) => event.uuid === uuid);
+    if (!event.setTitle(title)) {
+        result.error = event.getError();
+        res.status(400).json(result);
+        return;
+    }
 
-        if (foundIndex === -1) {
-            result.error = "Event not found";
-            res.status(404).json(result);
-            return;
-        }
+    if (!event.setDescription(description)) {
+        result.error = event.getError();
+        res.status(400).json(result);
+        return;
+    }
 
-        if ( !title ) {
-            result.error = "Title is missing";
-            res.status(400).json(result);
-            return;
-        }
+    if (!event.setLocation(location)) {
+        result.error = event.getError();
+        res.status(400).json(result);
+        return;
+    }
 
-        if ( title.length > titleLimit ) {
-            result.error = "Title can't be more than 20 characters long";
-            res.status(400).json(result);
-            return;
-        }
+    if (!event.setStart(start)) {
+        result.error = event.getError();
+        res.status(400).json(result);
+        return;
+    }
 
-        if ( !location ) {
-            result.error = "Location is missing";
-            res.status(400).json(result);
-            return;
-        }
+    if (!event.setTimeStart(timeStart)) {
+        result.error = event.getError();
+        res.status(400).json(result);
+        return;
+    }
 
-        if ( location.length > locationLimit ) {
-            result.error = "Location can't be more than 20 characters long";
-            res.status(400).json(result);
-            return;
-        }
+    if (!event.setEnd(end)) {
+        result.error = event.getError();
+        res.status(400).json(result);
+        return;
+    }
 
-        if ( description.length > descriptionLimit ) {
-            result.error = "Description can't be more than 500 characters long";
-            res.status(400).json(result);
-            return;
-        }
+    if (!event.setTimeEnd(timeEnd)) {
+        result.error = event.getError();
+        res.status(400).json(result);
+        return;
+    }
 
-        if ( !start ) {
-            result.error = "Starting Date is missing";
-            res.status(400).json(result);
-            return;
-        }
+    if (!event.setType(type)) {
+        result.error = event.getError();
+        res.status(400).json(result);
+        return;
+    }
 
-        if ( !isValidDateFormat(start) || !isValidDateFormat(end) ) {
-            result.error = "Dates formats are not valid";
-            res.status(400).json(result);
-            return;
-        }
+    if (!event.setUrl(eventUrl)) {
+        result.error = event.getError();
+        res.status(400).json(result);
+        return;
+    }
 
-        if ( !timeStart ) {
-            result.error = "Starting Time is missing";
-            res.status(400).json(result);
-            return;
-        }
+    if (!event.editEvent(deleteIndicator, imgArray)) {
+        result.error = event.getError();
+        res.status(500).json(result);
+        return;
+    }
 
-        if ( !end ) {
-            result.error = "Ending Date is missing";
-            res.status(400).json(result);
-            return;
-        }
-
-        if ( !timeEnd ) {
-            result.error = "Ending Time is missing";
-            res.status(400).json(result);
-            return;
-        }
-
-        if ( type !== "class" && type !== "meetup" && type !== "organization" ) {
-            result.error = "The type of the event is not valid (class, meetup or organization)";
-            res.status(400).json(result);
-            return;
-        }
-
-        if ( !type ) {
-            result.error = "Type is missing";
-            res.status(400).json(result);
-            return;
-        }
-        
-        let svImgArr = data[foundIndex].files;
-
-        if (deleteIndicator !== undefined) {
-            if ((imgArray.length - deleteIndicator.length) + data[foundIndex].files.length > 10) {
-                result.error = "Files cap for each event is settled to 10";
-                res.status(500).json(result);
-                return;
-            }
-            
-            if ( deleteIndicator.length > 1 ) {
-                deleteIndicator.map((indicator, index) => {
-                    const imgPath = path.join('./src/img/events', svImgArr[indicator]);
-
-                    svImgArr.splice(indicator, 1);
-                    unlink(imgPath, (err) => {
-                        if (err) {
-                            result.error = "Error deleting image file";
-                            res.status(500).json(result);
-                            return;
-                        }
-                    });
-                });
-            }
-
-            if (deleteIndicator.length === 1) {
-                const imgPath = path.join('./src/img/events', svImgArr[deleteIndicator]);
-
-                    svImgArr.splice(deleteIndicator, 1);
-                    unlink(imgPath, (err) => {
-                        if (err) {
-                            result.error = "Error deleting image file";
-                            res.status(500).json(result);
-                            return;
-                        }
-                    });
-            }
-        }
-
-        if ( imgArray.length == 0 ) {
-            imgArray = svImgArr;
-        } else {
-            imgArray = svImgArr.concat(imgArray);
-        }
-
-        const date1 = new Date(start);
-        const date2 = new Date(end);
-
-        if (date1 > date2) {
-            result.error = "Starting date can't be more than the ending date of the event";
-            res.status(500).json(result);
-            return;
-        }
-
-        const updatedEvent = {
-            ...data[foundIndex],
-            title: title,
-            description: description,
-            location: location,
-            start: start,
-            timeStart: timeStart,
-            end: end,
-            timeEnd: timeEnd,
-            type: type,
-            eventUrl: eventUrl,
-            files: imgArray
-        }
-
-        const updatedData = [...data];
-
-        updatedData[foundIndex] = updatedEvent;
-
-        const updatedJsonString = JSON.stringify(updatedData, null, 2);
-
-        writeFile("./src/json/events.json", updatedJsonString, err => {
-            if (err) {
-                result.error = "Error writing to the events file";
-                res.status(500).json(result);
-                return;
-            }
-
-            result.status = 1;
-            result.data = updatedEvent;
-
-            res.json(result);
-        })
-    });
+    result.status = 1;
+    result.data = uuid;
+    res.json(result);
 }
 
 export const deleteEvent = (req, res) => {
